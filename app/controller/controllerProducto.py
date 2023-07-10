1bf705fc258e6d0aaeaff63d7258268ed3b33bed
Bryan <bryan.castro.revilla@ucsp.edu.pe>
2023-07-09 19:54:45 -0500

    update ventas

--- app/controller/controllerProducto.py
+++ app/controller/controllerProducto.py
@@ -1,1 +1,14 @@
-# Funciones Productos
+from conexionBD import connectionBD
+
+def obtener_producto_por_id(id_producto):
+    conexion_MySQLdb = connectionBD()
+    cursor = conexion_MySQLdb.cursor(dictionary=True)
+
+    querySQL = "SELECT * FROM Producto WHERE ID_producto = %s"
+    cursor.execute(querySQL, (id_producto,))
+    producto = cursor.fetchone()
+
+    cursor.close()
+    conexion_MySQLdb.close()
+
+    return producto
