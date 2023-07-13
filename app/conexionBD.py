af73c06bc3fa5d0abb9456d10ee010c8f8e67ba3
Felix Gordillo <felix.gordillo@ucsp.edu.pe>
2023-07-12 20:02:25 -0500

    Nueva conexion

--- app/conexionBD.py
+++ app/conexionBD.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
 #Conectandose la base desplegada en la nube
-
+#NUEVA CONEXION:
 def connectionBD():
     mydb = mysql.connector.connect(
         host ="sql.freedb.tech",
-        user ="freedb_db_test_111",
-        passwd ="4JKtwGf@DQD@b?w",
-        database = "freedb_db_pharmacy"
+        user ="freedb_db_pharmacy_222",
+        passwd ="sT$w&z&Btw6CsJw",
+        database = "freedb_admin_test_1"
         )
     if mydb:
         print ("Conexion exitosa a BD")
